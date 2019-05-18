57175f85f5d7f2c073a0b7fd03e56335713d55b6
aka.あかつき <43906229+Aka-Akatsuki@users.noreply.github.com>
2019-05-18 21:56:45 +0900

    改善

--- myfunc/exchangeTop5js.php
+++ myfunc/exchangeTop5js.php
@@ -14,8 +14,15 @@
 <?php
   include ("../myfunc/commonfunction.php");
 
-  echo htmlspecialchars($_POST["exsrc"], ENT_QUOTES);
-
+  
+//  $befCode = htmlspecialchars($_POST["exsrc"], ENT_QUOTES);
+  
+$aftCode = $_POST["exsrc"];
+$aftCode = str_replace("int ","var ",$aftCode);
+$aftCode = str_replace("size( ","CreateCanvas( ",$aftCode);
+$aftCode = str_replace("void ","function ",$aftCode);
+ 
+echo "<textarea>", $aftCode , "</textarea>" ;
 
 ?> 
 
