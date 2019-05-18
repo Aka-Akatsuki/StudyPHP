77a60a8df5a896901c77e4ce4191cdb4f0fcd356
aka.あかつき <43906229+Aka-Akatsuki@users.noreply.github.com>
2019-05-18 21:36:06 +0900

    追加

---
+++ myfunc/exchangeTop5js-form.php
@@ -0,0 +1,28 @@
+<style>
+    textarea{
+        font-size: 20;
+        width: 30%;
+        height: 200px;
+
+    }
+</style>
+
+<html>
+<head> </head>
+<body>
+
+<?php
+  include ("../myfunc/commonfunction.php");
+
+?> 
+
+<form action="exchangeTop5js.php" method="POST">
+    ソースコードをここに入力します。 <br>
+    <textarea name="exsrc" row="10" col ="5">ここに入力</textarea> <br>
+    <input type="submit" value="変換"/>
+
+</form>
+
+
+</body>
+</html>
--- myfunc/exchangeTop5js.php
+++ myfunc/exchangeTop5js.php
@@ -1,9 +1,21 @@
+<style>
+    textarea{
+        font-size: 20;
+        width: 30%;
+        height: 200px;
+
+    }
+</style>
+
 <html>
 <head> </head>
 <body>
-
+変換後コード <br>
 <?php
   include ("../myfunc/commonfunction.php");
+
+  echo htmlspecialchars($_POST["exsrc"], ENT_QUOTES);
+
 
 ?> 
 
